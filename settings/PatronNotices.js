a5e258c5ae9a10d9cb0a0bedd28822ed49e59bb6
Matt Connolly <mjc12@cornell.edu>
2018-09-06 16:01:55 -0400

    Stub interface

--- settings/PatronNotices.js
+++ settings/PatronNotices.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import Button from '@folio/stripes-components/lib/Button';
+import Pane from '@folio/stripes-components/lib/Pane';
+import PaneMenu from '@folio/stripes-components/lib/PaneMenu';
 
 class PatronNotices extends React.Component {
   render() {
-    return <p>Placeholder</p>;
+    return (
+      <Pane
+        paneTitle="Patron notices"
+        lastMenu={<PaneMenu><Button buttonStyle="primary paneHeaderNewButton">+ New</Button></PaneMenu>}
+      >
+        <Link to="cornell.edu">General configuration</Link>
+      </Pane>
+    );
   }
 }
 
