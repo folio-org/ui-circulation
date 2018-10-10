28f25160d48a020cfa3defeb2841c55f0fab3b91
Matt Connolly <mjc12@cornell.edu>
2018-10-10 14:32:31 -0400

    Fix navigation between main and config panels

--- settings/PatronNoticesGeneralConfig.js
+++ settings/PatronNoticesGeneralConfig.js
@@ -6,12 +6,13 @@ import Pane from '@folio/stripes-components/lib/Pane';
 import PaneMenu from '@folio/stripes-components/lib/PaneMenu';
 
 class PatronNoticesGeneralConfig extends React.Component {
-
   render() {
     return (
       <Pane
         paneTitle="Patron notices | Configuration"
+        dismissible
         lastMenu={<PaneMenu><Button buttonStyle="primary paneHeaderNewButton">Save</Button></PaneMenu>}
+        onClose={() => this.props.history.push('/settings/circulation/patron-notices')}
       >
         <p>Test</p>
       </Pane>
