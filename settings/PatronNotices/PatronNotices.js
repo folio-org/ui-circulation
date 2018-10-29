462926a4cebc9f59e5cea7aac220e89c004362e0
Matt Connolly <mjc12@cornell.edu>
2018-10-29 07:31:22 -0500

    Make notices list a connected component

--- settings/PatronNotices/PatronNotices.js
+++ settings/PatronNotices/PatronNotices.js
@@ -14,13 +14,14 @@ class PatronNotices extends React.Component {
   constructor(props) {
     super(props);
     this.generalConfig = props.stripes.connect(PatronNoticesGeneralConfig);
+    this.noticesList = props.stripes.connect(PatronNoticesList);
   }
 
   render() {
     return (
       <Switch>
         <Route exact path="/settings/circulation/patron-notices/general" render={(props) => <this.generalConfig {...props} stripes={this.props.stripes} />} />
-        <Route component={PatronNoticesList} />
+        <Route component={this.noticesList} />
       </Switch>
     );
   }
