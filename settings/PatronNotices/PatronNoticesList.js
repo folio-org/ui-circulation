55619c843d871611775b2bbb87e04dbe003710d4
Matt Connolly <mjc12@cornell.edu>
2018-10-31 21:26:31 -0400

    Add initial manifest

--- settings/PatronNotices/PatronNoticesList.js
+++ settings/PatronNotices/PatronNoticesList.js
@@ -3,13 +3,46 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import Button from '@folio/stripes-components/lib/Button';
+import { makeQueryFunction } from '@folio/stripes/smart-components';
 import Pane from '@folio/stripes-components/lib/Pane';
 import PaneMenu from '@folio/stripes-components/lib/PaneMenu';
 
 import PatronNoticesGeneralConfig from './PatronNoticesGeneralConfig';
 
 class PatronNotices extends React.Component {
+  static manifest = Object.freeze({
+    addressTypes: {
+      type: 'okapi',
+      path: 'addresstypes',
+      records: 'addressTypes',
+    },
+    patronNoticeTemplates: {
+      type: 'okapi',
+      path: 'template',
+      records: 'templates',
+      // GET: {
+      //   params: {
+      //     query: makeQueryFunction(
+      //       'cql.allRecords=1',
+      //       '',
+      //     //  '(requester.barcode="${query.query}" or item.title="${query.query}" or title="${query.query}")',
+      //       {},
+      //     //  filterConfig,
+      //     )
+      //   },
+      //   staticFallback: { params: {} },
+      // },
+      // resourceShouldRefresh: true,
+    },
+  });
+
+  static propTypes = {
+    resources: PropTypes.object.isRequired,
+    stripes: PropTypes.object.isRequired,
+  };
+
   render() {
+    console.log("records", this.props.resources.patronNoticeTemplates)
     return (
       <Pane
         defaultWidth="fill"
