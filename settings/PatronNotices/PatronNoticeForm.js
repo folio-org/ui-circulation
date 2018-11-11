d3ca6cfbbf13ca3bd44f9c310ac62e2d8aa15a22
Matt Connolly <mjc12@cornell.edu>
2018-11-11 08:05:32 -0500

    Add initial pass at new edit form

--- settings/PatronNotices/PatronNoticeForm.js
+++ settings/PatronNotices/PatronNoticeForm.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Field } from 'redux-form';
+
+import {
+  Col,
+  Pane,
+  Paneset,
+  Row,
+  TextField,
+} from '@folio/stripes/components';
+import stripesForm from '@folio/stripes-form';
 
 class PatronNoticeForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.save = this.save.bind(this);
+  }
+
+  save(data) {
+    this.props.onSave(data);
+  }
+
   render() {
-    return <p>form</p>;
+    const { handleSubmit, initialValues } = this.props;
+    return (
+      <form id="form-patron-notice" onSubmit={handleSubmit(this.save)}>
+        <Paneset isRoot>
+          <Pane defaultWidth="100%">
+            <Row>
+              <Col xs={8}>
+                <Field label="name" name="name" id="input-patron-notice-name" component={TextField} />
+              </Col>
+            </Row>
+          </Pane>
+        </Paneset>
+      </form>
+    );
   }
 }
 
-export default PatronNoticeForm;
+export default stripesForm({
+  form: 'patronNoticeForm',
+  navigationCheck: true,
+  enableReinitialize: false,
+})(PatronNoticeForm);
