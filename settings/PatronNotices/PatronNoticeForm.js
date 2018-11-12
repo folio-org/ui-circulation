cfca4d64e2aa29c69bbf37c2211841a37d655c26
Matt Connolly <mjc12@cornell.edu>
2018-11-11 19:32:28 -0500

    Flesh out edit/create form

--- settings/PatronNotices/PatronNoticeForm.js
+++ settings/PatronNotices/PatronNoticeForm.js
@@ -1,38 +1,182 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { find } from 'lodash';
 import { Field } from 'redux-form';
 
 import {
+  Accordion,
+  AccordionSet,
+  Button,
+  Checkbox,
   Col,
+  IconButton,
   Pane,
+  PaneMenu,
   Paneset,
   Row,
+  TextArea,
   TextField,
 } from '@folio/stripes/components';
 import stripesForm from '@folio/stripes-form';
+
+import PatronNoticeEditor from './PatronNoticeEditor';
 
 class PatronNoticeForm extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      accordions: {
+        'email-template': true,
+        'sms-template': true,
+        'print-template': true,
+      },
+    };
+
+    this.onToggleSection = this.onToggleSection.bind(this);
     this.save = this.save.bind(this);
+  }
+
+  onToggleSection({ id }) {
+    this.setState((curState) => {
+      const newState = _.cloneDeep(curState);
+      newState.accordions[id] = !curState.accordions[id];
+      return newState;
+    });
   }
 
   save(data) {
     this.props.onSave(data);
   }
 
+  renderCLoseIcon() {
+    return (
+      <PaneMenu>
+        <IconButton
+          id="clickable-close-patron-notice"
+          onClick={this.props.onCancel}
+          icon="closeX"
+          title="close"
+          aria-label="Close"
+        />
+      </PaneMenu>
+    );
+  }
+
+  renderPaneTitle() {
+    const { initialValues } = this.props;
+    const notice = initialValues || {};
+
+    // If there's an ID, this is editing an existing notice
+    if (notice.id) {
+      return (<span>Patron notices | {notice.name}</span>);
+    }
+
+    return <span>Patron notices</span>
+  }
+
+  renderSaveMenu() {
+    const { pristine, submitting, initialValues } = this.props;
+    const editing = initialValues && initialValues.id;
+    const saveLabel = editing ? 'Save' : 'Save new';
+
+    return (
+      <PaneMenu>
+        <Button
+          id="clickable-save-patron-notice"
+          type="submit"
+          title='Save'
+          buttonStyle="primary paneHeaderNewButton"
+          marginBottom0
+          disabled={(pristine || submitting)}
+        >
+          {saveLabel}
+        </Button>
+      </PaneMenu>
+    );
+  }
+
   render() {
     const { handleSubmit, initialValues } = this.props;
+    const notice = this.props.initialValues;
+    const emailTemplate = find(notice.localizedTemplates, { 'header': 'email' });
+    const smsTemplate = find(notice.localizedTemplates, { 'header': 'sms' });
+    const printTemplate = find(notice.localizedTemplates, { 'header': 'print' });
+
     return (
       <form id="form-patron-notice" onSubmit={handleSubmit(this.save)}>
         <Paneset isRoot>
-          <Pane defaultWidth="100%">
+          <Pane defaultWidth="100%" paneTitle={this.renderPaneTitle()} firstMenu={this.renderCLoseIcon()} lastMenu={this.renderSaveMenu()}>
             <Row>
               <Col xs={8}>
-                <Field label="name" name="name" id="input-patron-notice-name" component={TextField} />
+                <Field label="Name" name="name" id="input-patron-notice-name" component={TextField} />
+              </Col>
+              <Col xs={3}>
+                <Field label="Active" name="active" id="input-patron-notice-active" component={Checkbox} />
               </Col>
             </Row>
+            <Row>
+              <Col xs={8}>
+                <Field label="Description" name="description" id="input-patron-notice-description" component={TextArea} />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={8}>
+                <Field label="Category" name="category" id="input-patron-notice-category" component={TextField} />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={8}>
+                <Field label="Subject" name="subject" id="input-patron-notice-subject" component={TextField} />
+              </Col>
+            </Row>
+            <AccordionSet accordionStatus={this.state.accordions} onToggle={this.onToggleSection}>
+              <Accordion
+                id="email-template"
+                label="Email"
+              >
+                { emailTemplate &&
+                  <div>
+                    <Row>
+                      <Button>Preview</Button>
+                    </Row>
+                    <Row>
+                      {/* <Field label="Body" name="subject" id="input-email-template-subject" component={PatronNoticeEditor} /> */}
+                    </Row>
+                  </div>
+                }
+              </Accordion>
+              <Accordion
+                id="sms-template"
+                label="SMS"
+              >
+                { smsTemplate &&
+                  <div>
+                    <Row>
+                      <Button>Preview</Button>
+                    </Row>
+                    <Row>
+                      {/* <Field label="Subject" name="subject" id="input-email-template-subject" component={TextField} /> */}
+                    </Row>
+                  </div>
+                }
+              </Accordion>
+              <Accordion
+                id="print-template"
+                label="Print"
+              >
+                { printTemplate &&
+                  <div>
+                    <Row>
+                      <Button>Preview</Button>
+                    </Row>
+                    <Row>
+                      {/* <Field label="Subject" name="subject" id="input-email-template-subject" component={TextField} /> */}
+                    </Row>
+                  </div>
+                }
+              </Accordion>
+            </AccordionSet>
           </Pane>
         </Paneset>
       </form>
