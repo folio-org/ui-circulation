77417694609eafff6994225b8312b27b000db9fc
Michal Kuklis <michal.kuklis@gmail.com>
2019-01-14 20:11:39 -0500

    Cleanup

--- test/bigtest/network/scenarios/default.js
+++ test/bigtest/network/scenarios/default.js
@@ -2,9 +2,5 @@
 
 // default scenario is used during `yarn start --mirage`
 export default function defaultScenario(server) {
-  server.create('user', {
-    barcode: 9676761472500
-  });
 
-  server.create('request');
 }
