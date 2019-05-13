053b5517a284717935efca44def5f9916fa7eaf5
Michal Kuklis <michal.kuklis@gmail.com>
2019-05-13 11:58:09 -0400

    Added extra tests

--- test/bigtest/tests/circulation-rules-test.js
+++ test/bigtest/tests/circulation-rules-test.js
@@ -7,10 +7,15 @@ import { toLowercaseReplaceAllSpaces } from '../helpers/messageСonverters';
 
 describe('CirculationRules', () => {
   setupApplication();
+
   let loanPolicies;
+  let requestPolicies;
+  let patronNoticePolicies;
 
   beforeEach(async function () {
     loanPolicies = await this.server.createList('loanPolicy', 3);
+    requestPolicies = await this.server.createList('requestPolicy', 3);
+    patronNoticePolicies = await this.server.createList('patronNoticePolicy', 3);
 
     return this.visit('/settings/circulation/rules', () => {
       expect(circulationRules.$root).to.exist;
@@ -86,6 +91,36 @@ describe('CirculationRules', () => {
       expect(circulationRules.editor.value).to.be.equal(
         `fallback-policy: l ${toLowercaseReplaceAllSpaces(loanPolicies[0].name)} `
       );
+    });
+  });
+
+  describe('saving circulation rules', () => {
+    let savedRules;
+    let lPolicy;
+    let rPolicy;
+    let nPolicy;
+
+    beforeEach(async function () {
+      lPolicy = loanPolicies[0];
+      rPolicy = requestPolicies[0];
+      nPolicy = patronNoticePolicies[0];
+
+      const lName = toLowercaseReplaceAllSpaces(lPolicy.name);
+      const rName = toLowercaseReplaceAllSpaces(rPolicy.name);
+      const nName = toLowercaseReplaceAllSpaces(nPolicy.name);
+
+      this.server.put('/circulation/rules', (_, request) => {
+        const params = JSON.parse(request.requestBody);
+        savedRules = params.rulesAsText;
+        return params;
+      });
+
+      await circulationRules.editor.setValue(`m book dvd: l ${lName} r ${rName} n ${nName}`);
+      await circulationRules.clickSaveRulesBtn();
+    });
+
+    it('should choose loan policy as a fallback', () => {
+      expect(savedRules).to.be.equal(`m 1a54b431-2e4f-452d-9cae-9cee66c9a892 5ee11d91-f7e8-481d-b079-65d708582ccc: l ${lPolicy.id} r ${rPolicy.id} n ${nPolicy.id}`);
     });
   });
 });
