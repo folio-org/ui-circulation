65be0d1d2c0d3e1a159b34d23a9a6da401531e66
Michal Kuklis <michal.kuklis@gmail.com>
2019-05-15 10:57:50 -0400

    Add more tests for staff slips

--- test/bigtest/network/serializers/staff-slip.js
+++ test/bigtest/network/serializers/staff-slip.js
@@ -3,7 +3,6 @@ import ApplicationSerializer from './application';
 export default ApplicationSerializer.extend({
   serialize(...args) {
     const json = ApplicationSerializer.prototype.serialize.apply(this, args);
-    console.log(json);
     json.totalRecords = json.staffSlips.length;
     return json;
   }
