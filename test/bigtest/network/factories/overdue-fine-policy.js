d90262232e985b8396969374dd0ddc0f968acb35
DGB2018 <lmanillah@dgb.unam.mx>
2019-09-03 22:29:11 -0500

    UIU-1146: BigTest CRUD Fee/Fine Overdue Fine Policies

---
+++ test/bigtest/network/factories/overdue-fine-policy.js
@@ -0,0 +1,29 @@
+import {
+  Factory,
+  faker,
+} from '@bigtest/mirage';
+
+export default Factory.extend({
+  id: () => faker.random.uuid(),
+  name: () => faker.hacker.noun(),
+  description: () => faker.company.catchPhrase(),
+  countClosed: () => faker.random.boolean(),
+  forgiveOverdueFine: () => faker.random.boolean(),
+  gracePeriodRecall: () => faker.random.boolean(),
+  maxOverdueFine: () => faker.random.number(),
+  maxOverdueRecallFine: () => faker.random.number(),
+  overdueFine: {
+    quantity: () => faker.random.number(),
+    intervalId: () => faker.hacker.noun(),
+  },
+  overdueRecallFine: {
+    quantity: () => faker.random.number(),
+    intervalId: () => faker.hacker.noun(),
+  },
+  metadata: {
+    createdDate: faker.date.past(0.1, faker.date.past(0.1)).toString(),
+    createdByUserId: faker.random.uuid(),
+    updatedDate: faker.date.past(0.1).toString(),
+    updatedByUserId: faker.random.uuid(),
+  }
+});
---
+++ test/bigtest/tests/fine-policy/fine-policy-form-test.js
@@ -0,0 +1,113 @@
+import { beforeEach, describe, it } from '@bigtest/mocha';
+import { expect } from 'chai';
+
+import setupApplication from '../../helpers/setup-application';
+import FinePolicyForm from '../../interactors/fine-policy/fine-policy-form';
+import FinePolicyDetail from '../../interactors/fine-policy/fine-policy-detail';
+
+describe('FinePolicyForm', () => {
+  setupApplication({ scenarios: ['testFinePolicy'] });
+
+  describe('accordions', () => {
+    beforeEach(function () {
+      this.visit('/settings/circulation/fine-policies?layer=add');
+    });
+
+    it('should be displayed', () => {
+      expect(FinePolicyForm.expandAll.isPresent).to.be.true;
+    });
+
+    describe('collapse all', () => {
+      beforeEach(async () => {
+        await FinePolicyForm.overdueAccordion.clickHeader()
+          .overdueAccordion.clickHeader()
+          .expandAll.click();
+      });
+
+      it('aboutSection should not be displayed', () => {
+        expect(FinePolicyForm.aboutSection.isHidden).to.be.true;
+      });
+
+      it('overdueFineSection should not be displayed', () => {
+        expect(FinePolicyForm.overdueFineSection.isHidden).to.be.true;
+      });
+
+      describe('expand all', () => {
+        beforeEach(async () => {
+          await FinePolicyForm.expandAll.click();
+        });
+
+        it('aboutSection should be displayed', () => {
+          expect(FinePolicyForm.aboutSection.isVisible).to.be.true;
+        });
+
+        it('overdueFineSection should be displayed', () => {
+          expect(FinePolicyForm.overdueFineSection.isVisible).to.be.true;
+        }).timeout(3000);
+      });
+    });
+  });
+
+  describe('create a new fine policy', () => {
+    beforeEach(function () {
+      this.visit('/settings/circulation/fine-policies?layer=add');
+    });
+
+    describe('filling form and saving new policy', () => {
+      const newFinePolicyName = 'new fine policy';
+
+      beforeEach(async function () {
+        await FinePolicyForm
+          .aboutSection.policyName.fillAndBlur(newFinePolicyName)
+          .save();
+      });
+
+      it('renders updated policy name', function () {
+        expect(FinePolicyDetail.aboutSection.policyName.value.text).to.equal(newFinePolicyName);
+      });
+    });
+  });
+
+  describe('create a new fine policy without overdue fine', () => {
+    beforeEach(function () {
+      this.visit('/settings/circulation/fine-policies?layer=add');
+    });
+
+    describe('filling form and saving new policy', () => {
+      const newFinePolicyName = 'new fine policy2';
+
+      beforeEach(async function () {
+        await FinePolicyForm
+          .aboutSection.policyName.fillAndBlur(newFinePolicyName)
+          .overdueFineSection.overdue.quantity.fillValue('9')
+          .overdueFineSection.overdue.quantity.blurInput()
+          .overdueFineSection.overdue.interval.selectAndBlur('day')
+          .overdueFineSection.maxOverdue.fillAndBlur('11.99')
+          .save();
+      });
+
+      it('renders updated policy name', function () {
+        expect(FinePolicyDetail.aboutSection.policyName.value.text).to.equal(newFinePolicyName);
+      });
+    });
+
+    describe('filling form and saving new policy', () => {
+      const newFinePolicyName = 'new fine policy3';
+
+      beforeEach(async function () {
+        await FinePolicyForm
+          .aboutSection.policyName.fillAndBlur(newFinePolicyName)
+          .overdueFineSection.overdue.quantity.fillValue('0.00')
+          .overdueFineSection.overdue.quantity.blurInput()
+          .overdueFineSection.overdue.interval.selectAndBlur('day')
+          .overdueFineSection.maxOverdue.fillAndBlur('0')
+          .overdueFineSection.maxOverdue.blurInput()
+          .save();
+      });
+
+      it('renders updated policy name', function () {
+        expect(FinePolicyDetail.aboutSection.policyName.value.text).to.equal(newFinePolicyName);
+      });
+    });
+  });
+});
