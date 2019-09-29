d288fc186a557d3420cf319276171d7e76cd32d5
DGB2018 <lmanillah@dgb.unam.mx>
2019-09-29 09:36:16 -0500

    Remove only

--- test/bigtest/tests/lost-item-fee-policy/lost-item-fee-policy-detail-test.js
+++ test/bigtest/tests/lost-item-fee-policy/lost-item-fee-policy-detail-test.js
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 import setupApplication from '../../helpers/setup-application';
 import LostItemFeePolicyDetail from '../../interactors/lost-item-fee-policy/lost-item-fee-policy-detail';
 
-describe.only('LostItemFeePolicyDetail', () => {
+describe('LostItemFeePolicyDetail', () => {
   setupApplication({ scenarios: ['testLostItemFeePolicy'] });
 
   let lostItemPolicy;
