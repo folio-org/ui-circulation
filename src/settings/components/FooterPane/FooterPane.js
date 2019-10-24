bfb6614123925d30c8d6fd2c0349fd2c47952b22
Maxim Didenko <maxim_didenko@epam.com>
2019-10-24 18:13:48 +0300

    UICIRC-367 - added missed files

---
+++ src/settings/components/FooterPane/FooterPane.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+
+import {
+  Button,
+  PaneFooter,
+} from '@folio/stripes/components';
+
+import css from './FooterPane.css';
+
+const FooterPane = (props) => {
+  const {
+    pristine,
+    submitting,
+    onCancel,
+  } = props;
+
+  return (
+    <PaneFooter>
+      <div className={css.footerContent}>
+        <Button
+          id="footer-cancel-entity"
+          marginBottom0
+          onClick={onCancel}
+        >
+          <FormattedMessage id="ui-circulation.settings.common.cancel" />
+        </Button>
+        <Button
+          id="footer-save-entity"
+          type="submit"
+          marginBottom0
+          buttonStyle="primary"
+          disabled={(pristine || submitting)}
+        >
+          <FormattedMessage id="ui-circulation.settings.common.saveAndClose" />
+        </Button>
+      </div>
+    </PaneFooter>
+  );
+};
+
+FooterPane.propTypes = {
+  pristine: PropTypes.bool.isRequired,
+  submitting: PropTypes.bool.isRequired,
+  onCancel: PropTypes.func.isRequired,
+};
+
+export default FooterPane;
