5f2a7aa42b499c2ee9adbae90b742fec93156518
Maxim Didenko <maxim_didenko@epam.com>
2020-04-07 12:35:41 +0300

    UICIRC-451 - fixed linter warnings.

--- src/settings/FinePolicy/FinePolicyForm.js
+++ src/settings/FinePolicy/FinePolicyForm.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getFormValues } from 'redux-form';
 import stripesForm from '@folio/stripes/form';
--- src/settings/Validation/notice-policy/notice-policy.js
+++ src/settings/Validation/notice-policy/notice-policy.js
@@ -2,7 +2,7 @@ import FormValidator from '../engine/FormValidator';
 import general from './general';
 import loanNotices from './loan-notices';
 import requestNotices from './request-notices';
-import feeFineNotices from './fee-fine-notices'
+import feeFineNotices from './fee-fine-notices';
 import { NoticePolicy } from '../../Models/NoticePolicy';
 
 export default function (policy) {
