cc2734fbf3488c442a3e69b12930804bccd6b0b4
Maxim Didenko <maxim_didenko@epam.com>
2020-08-12 14:09:37 +0300

    UICIRC-442 - move overdue fine policy form to react-final-form

---
+++ src/settings/FinePolicy/utils/normalize.js
@@ -0,0 +1,26 @@
+import {
+  isEmpty,
+  isUndefined,
+  cloneDeep,
+} from 'lodash';
+
+const isZeroOrBlank = (period) => {
+  const isZero = !isUndefined(period) && Number(period.quantity) === 0;
+  const isBlank = !isUndefined(period) && isEmpty(period.quantity);
+
+  return isZero || isBlank;
+};
+
+export default (policy) => {
+  const finePolicy = cloneDeep(policy);
+
+  if (isZeroOrBlank(finePolicy.overdueFine)) {
+    delete finePolicy.overdueFine;
+  }
+
+  if (isZeroOrBlank(finePolicy.overdueRecallFine)) {
+    delete finePolicy.overdueRecallFine;
+  }
+
+  return finePolicy;
+};
