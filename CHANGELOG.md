2dedd699ca2ee9e0be9062ab82ae12ac26f22336
Stanislau Snapok <stanislau_snapok@epam.com>
2021-09-27 10:06:22 +0300

    UICIRC-651: Add RTL/Jest tests for `TokensList` component

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -44,6 +44,7 @@
 * Add RTL/Jest testing for `GeneralSection` component in `NoticePolicy/components/EditSections`. Refs UICIRC-638.
 * Add RTL/Jest testing for `GeneralSection` component in `RequestPolicy/components/EditSections`. Refs UICIRC-648.
 * Add RTL/Jest testing for `LostItemFeeSection` component in `LostItemFeePolicy/components/ViewSections/LostItemFeeSection`. Refs UICIRC-627.
+* Add RTL/Jest testing for `TokensList` component in `settings/StaffSlips/TokensList`. Refs UICIRC-651.
 
 ## [5.1.0] (https://github.com/folio-org/ui-circulation/tree/v5.1.0) (2021-06-14)
 [Full Changelog](https://github.com/folio-org/ui-circulation/compare/v5.0.1...v5.1.0)
---
+++ src/settings/StaffSlips/TokensList/TokensList.test.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {
+  render,
+} from '@testing-library/react';
+
+import '../../../../test/jest/__mock__';
+
+import { TokensSection } from '@folio/stripes-template-editor';
+import TokensList from './TokensList';
+
+jest.mock('@folio/stripes-template-editor', () => ({
+  TokensSection: jest.fn(() => null),
+}));
+
+describe('TokensList', () => {
+  const mockedTokens = {
+    item: 'testItem',
+    effectiveLocation: 'testEffectiveLocation',
+    staffSlip: 'testStaffSlip',
+    request: 'testRequest',
+    requestDeliveryAddress: 'testRequestDeliveryAddress',
+    requester: 'testRequester',
+  };
+  const mockedOnSectionInit = jest.fn();
+  const mockedOnTokenSelect = jest.fn();
+
+  const sectionNamesForTest = [
+    'item',
+    'effectiveLocation',
+    'staffSlip',
+    'request',
+    'requestDeliveryAddress',
+    'requester',
+  ];
+
+  const tokensSectionTest = (name, index) => {
+    it(`should execute "TokensSection" in "${name}" section with passed props`, () => {
+      const number = index + 1;
+      const expectedResult = {
+        section: name,
+        header: `ui-circulation.settings.staffSlips.${name}TokenHeader`,
+        tokens: mockedTokens[name],
+        onSectionInit: mockedOnSectionInit,
+        onTokenSelect: mockedOnTokenSelect,
+      };
+
+      expect(TokensSection).toHaveBeenNthCalledWith(number, expectedResult, {});
+    });
+  };
+
+  beforeEach(() => {
+    render(
+      <TokensList
+        tokens={mockedTokens}
+        onSectionInit={mockedOnSectionInit}
+        onTokenSelect={mockedOnTokenSelect}
+      />
+    );
+  });
+
+  afterEach(() => {
+    TokensSection.mockClear();
+  });
+
+  sectionNamesForTest.forEach(tokensSectionTest);
+});
