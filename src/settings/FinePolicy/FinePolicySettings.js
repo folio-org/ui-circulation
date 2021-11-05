ee4acb7bcd05ff547d6699ceb0dfa81364167c7a
Matthew Connolly <mjc12@cornell.edu>
2021-11-05 14:54:04 -0400

    Don't allow fine policies to be deleted if they're in use

--- src/settings/FinePolicy/FinePolicySettings.js
+++ src/settings/FinePolicy/FinePolicySettings.js
@@ -17,6 +17,7 @@ import { MAX_UNPAGED_RESOURCE_COUNT } from '../../constants';
 
 class FinePolicySettings extends React.Component {
   static manifest = Object.freeze({
+    selectedPolicyId: {},
     finePolicies: {
       type: 'okapi',
       records: 'overdueFinePolicies',
@@ -27,7 +28,14 @@ class FinePolicySettings extends React.Component {
       },
       throwErrors: false,
     },
-
+    loans: {
+      type: 'okapi',
+      records: 'loans',
+      path: 'circulation/loans',
+      params: {
+        query: 'status.name==Open and overdueFinePolicyId==%{selectedPolicyId}',
+      },
+    },
   });
 
   static propTypes = {
@@ -42,8 +50,34 @@ class FinePolicySettings extends React.Component {
         PUT: PropTypes.func.isRequired,
         DELETE: PropTypes.func.isRequired,
       }),
+      selectedPolicyId: PropTypes.shape({
+        replace: PropTypes.func.isRequired,
+      }),
     }).isRequired,
   };
+
+  constructor(props) {
+    super(props);
+
+    this.updateSelectedPolicy = this.updateSelectedPolicy.bind(this);
+    this.updateSelectedPolicy();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.location.pathname !== this.props.location.pathname) {
+      this.updateSelectedPolicy();
+    }
+  }
+
+  updateSelectedPolicy() {
+    const {
+      location,
+      mutator,
+    } = this.props;
+
+    const path = location.pathname;
+    mutator.selectedPolicyId.replace(path.substring(path.lastIndexOf('/') + 1));
+  }
 
   parseInitialValues = (init = {}) => ({
     ...init,
@@ -58,6 +92,17 @@ class FinePolicySettings extends React.Component {
       quantity: parseFloat((init.overdueRecallFine || {}).quantity || 0).toFixed(2)
     }
   });
+
+  /**
+   * This function is used as a prop for <EntryManager> to determine
+   * whether or not a policy can be deleted -- i.e., whether or not
+   * there are any active loans that are using it.
+   *
+   * The loans resource query returns open loan records with a loanPolicyId
+   * of `selectedPolicyId`, a local resource that should be set before
+   * calling this function.
+   */
+  isPolicyInUse = () => this.props.resources.loans.isLoading || this.props.resources.loans.records.length > 0;
 
   render() {
     const {
@@ -86,9 +131,15 @@ class FinePolicySettings extends React.Component {
         permissions={permissions}
         parseInitialValues={this.parseInitialValues}
         parentResources={resources}
+        prohibitItemDelete={{
+          close: <FormattedMessage id="ui-circulation.settings.common.close" />,
+          label: <FormattedMessage id="ui-circulation.settings.finePolicy.denyDelete.header" />,
+          message: <FormattedMessage id="ui-circulation.settings.policy.denyDelete.body" />,
+        }}
         detailComponent={FinePolicyDetail}
         enableDetailsActionMenu
         entryFormComponent={FinePolicyForm}
+        isEntryInUse={this.isPolicyInUse}
         paneTitle={<FormattedMessage id="ui-circulation.settings.finePolicy.paneTitle" />}
         entryLabel={formatMessage({ id: 'ui-circulation.settings.finePolicy.entryLabel' })}
         defaultEntry={FinePolicy.defaultFinePolicy()}
