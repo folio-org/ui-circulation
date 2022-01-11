30f820341b9195e529b40550051119ca72967dcf
Spichonak Pavel <Pavel_Spichonak@epam.com>
2022-01-11 16:12:53 +0300

    UICIRC-600: UI tests replacement with RTL/Jest for FixedDueDateScheduleManager

--- test/jest/__mock__/stripesSmartComponents.mock.js
+++ test/jest/__mock__/stripesSmartComponents.mock.js
@@ -5,4 +5,5 @@ jest.mock('@folio/stripes/smart-components', () => ({
   ViewMetaData: jest.fn(({ metadata, ...rest }) => (
     <div {...rest}>{metadata.createdDate}</div>
   )),
+  EntryManager: jest.fn(() => null),
 }));
