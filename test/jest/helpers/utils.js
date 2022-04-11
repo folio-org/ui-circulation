f3409ecf83272906c76429462c8fa8f9c13f1b85
Stanislav <88130496+Stanislau-Snapok@users.noreply.github.com>
2022-04-11 16:16:21 +0300

    UICIRC-636: Add Jest/RTL tests for `NoticeCard` component (#900)

---
+++ test/jest/helpers/utils.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/prefer-default-export */
+export const componentPropsCheck = (Component, testId, expectedProps, partialCompare = false) => {
+  const propertiesForCompare = Component.mock.calls
+    .reverse()
+    .find(item => item[0]['data-testid'] === testId);
+
+  const resultExpectedProps = partialCompare
+    ? expect.objectContaining(expectedProps)
+    : {
+      ...expectedProps,
+      'data-testid': testId,
+    };
+
+  expect(propertiesForCompare[0]).toStrictEqual(resultExpectedProps);
+};
