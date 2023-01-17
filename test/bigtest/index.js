24326e57b1a0b50aa4a080a2e808d47e1395c001
Dmitriy-Litvinenko <Dmitriy_Litvinenko@epam.com>
2023-01-17 14:08:08 +0200

    UICIRC-591: Remove bigtest tests and associated dependencies

--- test/bigtest/index.js
+++
@@ -1,9 +0,0 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-
-import './helpers/monkey-patch-run';
-
-// require all modules ending in "-test" from the current directory and
-// all subdirectories
-const requireTest = require.context('./tests/', true, /-test/);
-requireTest.keys().forEach(requireTest);
--- test/bigtest/network/factories/staff-slip.js
+++
@@ -1,9 +0,0 @@
-import { Factory } from 'miragejs';
-import faker from 'faker';
-
-export default Factory.extend({
-  name: () => 'Hold',
-  description: () => faker.company.catchPhrase(),
-  active: true,
-  template: '<p>{{Item barcode}}</p>',
-});
