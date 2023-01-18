0a64672b80f5a018cf2a1cc50992396a316b1733
Dmitriy-Litvinenko <Dmitriy_Litvinenko@epam.com>
2023-01-18 16:10:32 +0200

    UICIRC-591: Remove bigtest tests and associated dependencies (#984)
    UICIRC-591: Remove bigtest tests and associated dependencies

--- test/bigtest/network/factories/campus.js
+++
@@ -1,9 +0,0 @@
-import { Factory } from 'miragejs';
-import faker from 'faker';
-
-export default Factory.extend({
-  id: faker.random.uuid,
-  name: faker.hacker.noun(),
-  code: faker.address.stateAbbr,
-  institutionId: faker.random.uuid,
-});
