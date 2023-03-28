9dee69467774fb3072776985d8608dd66ee36e66
Priyanka <priyanka_terala@epam.com>
2023-03-28 17:00:31 +0530

    UICIRC-844 - refine small bits

--- src/settings/StaffSlips/tokens.js
+++ src/settings/StaffSlips/tokens.js
@@ -2,242 +2,243 @@ import { staffSlipMap } from '../../constants';
 import { generatePreviewDateValue } from '../PatronNotices/utils';
 import { DATE_FORMAT_WITH_TIME } from '../PatronNotices/utils/constantsForMoment';
 
+const allowedForAllStaffSlips = [...Object.values(staffSlipMap)];
 const getTokens = (locale) => ({
   item: [
     {
       token: 'item.title',
       previewValue: 'The Wines of Italy',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.primaryContributor',
       previewValue: 'Thomas, George B.',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.allContributors',
       previewValue: 'Finney, Ross L.; Weir, Maurice D.',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.barcode',
       previewValue: '31924001521792',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.barcodeImage',
       previewValue: '<Barcode>31924001521792</Barcode>',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.callNumber',
       previewValue: 'TK7871.15.F4 S67 1988',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.callNumberPrefix',
       previewValue: 'New & Noteworthy',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.callNumberSuffix',
       previewValue: 'Handbook',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.copy',
       previewValue: 'c.2',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.enumeration',
       previewValue: 'no.1-3',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.volume',
       previewValue: 'v. 27',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.chronology',
       previewValue: '1964-1967 (Board)',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.yearCaption',
       previewValue: 'Convention photographs 1911-1960',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.loanType',
       previewValue: 'Course reserves',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.materialType',
       previewValue: 'Serial',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.numberOfPieces',
       previewValue: '7',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.descriptionOfPieces',
       previewValue: '7 maps in pocket',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.lastCheckedInDateTime',
       previewValue: generatePreviewDateValue(locale, DATE_FORMAT_WITH_TIME),
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.fromServicePoint',
       previewValue: 'Circulation Desk - South Library',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.toServicePoint',
       previewValue: 'Circulation Desk - Main Library',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
   ],
   effectiveLocation: [
     {
       token: 'item.effectiveLocationInstitution',
       previewValue: 'Opentown University',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.effectiveLocationCampus',
       previewValue: 'South Campus',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.effectiveLocationLibrary',
       previewValue: 'Main Library',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'item.effectiveLocationSpecific',
       previewValue: 'Main Library Reserve',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
   ],
   request: [
     {
       token: 'request.servicePointPickup',
       previewValue: 'Circulation Desk - Main Library',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'request.requestExpirationDate',
       previewValue: generatePreviewDateValue(locale, DATE_FORMAT_WITH_TIME),
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'request.holdShelfExpirationDate',
       previewValue: generatePreviewDateValue(locale, DATE_FORMAT_WITH_TIME),
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'request.requestID',
       previewValue: '987321654',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'request.patronComments',
       previewValue: 'Please deliver to the History building for Professor McCoy.',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
   ],
   requestDeliveryAddress: [
     {
       token: 'request.deliveryAddressType',
       previewValue: 'Office',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'requester.addressLine1',
       previewValue: '123 Campus Drive',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'requester.addressLine2',
       previewValue: '226 Classroom Building, Box 12345',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'requester.city',
       previewValue: 'Los Angeles',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'requester.stateProvRegion',
       previewValue: 'CA',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'requester.zipPostalCode',
       previewValue: '90048',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'requester.country',
       previewValue: 'USA',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
   ],
   requester: [
     {
       token: 'requester.firstName',
       previewValue: 'James',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'requester.lastName',
       previewValue: 'Smith',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'requester.middleName',
       previewValue: 'Adam',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'requester.preferredFirstName',
       previewValue: 'Paul',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'requester.barcode',
       previewValue: '456123789',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'requester.barcodeImage',
       previewValue: '<Barcode>456123789</Barcode>',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'requester.patronGroup',
       previewValue: 'Undergraduate',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
     {
       token: 'requester.departments',
       previewValue: 'Library Technical Services; IT Operations',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
   ],
   staffSlip: [
     {
       token: 'staffSlip.Name',
       previewValue: 'Transit',
-      allowedFor: [...Object.values(staffSlipMap)],
+      allowedFor: allowedForAllStaffSlips,
     },
   ],
 });
