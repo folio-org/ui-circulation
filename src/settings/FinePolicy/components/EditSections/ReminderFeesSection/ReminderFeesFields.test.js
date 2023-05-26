a2f66fbb999f2794032133f3b7be49d038d74598
Michal Kuklis <michal.kuklis@gmail.com>
2023-05-25 20:38:49 -0400

    Add test for ReminderFeesFields

---
+++ src/settings/FinePolicy/components/EditSections/ReminderFeesSection/ReminderFeesFields.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import '../../../../../../test/jest/__mock__';
+
+import { FieldArray } from 'react-final-form-arrays';
+
+import ReminderFeesFields from './ReminderFeesFields';
+
+describe('ReminderFeesFields', () => {
+  afterEach(() => {
+    FieldArray.mockClear();
+  });
+
+  beforeEach(() => {
+    render(<ReminderFeesFields />);
+  });
+
+  it('should execute "FieldArray" with passed props', () => {
+    const expectedProps = {
+      name: 'reminderFeesPolicy.reminderSchedule'
+    };
+
+    expect(FieldArray).toHaveBeenLastCalledWith(expect.objectContaining(expectedProps), {});
+  });
+});
