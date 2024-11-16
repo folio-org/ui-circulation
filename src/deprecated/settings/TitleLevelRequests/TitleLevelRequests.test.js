29d57551eb3ac3187629bf982f32a7a76e20fa74
Dmitriy-Litvinenko <Dmitriy_Litvinenko@epam.com>
2024-11-16 14:08:49 +0200

    UICIRC-1118: Implement feature toggle for ECS and not ECS envs

---
+++ src/deprecated/settings/TitleLevelRequests/TitleLevelRequests.test.js
@@ -0,0 +1,68 @@
+import {
+  render,
+} from '@folio/jest-config-stripes/testing-library/react';
+import { ConfigManager } from '@folio/stripes/smart-components';
+import { TitleManager } from '@folio/stripes/core';
+
+import TitleLevelRequests, {
+  CONFIG_NAMES,
+} from './TitleLevelRequests';
+import TitleLevelRequestsForm from './TitleLevelRequestsForm';
+import {
+  getInitialValues,
+  normalizeData,
+} from './utils';
+import {
+  MODULE_NAMES,
+} from '../../../constants';
+
+describe('deprecated TitleLevelRequests', () => {
+  const paneTitleLabelId = 'ui-circulation.settings.titleLevelRequests.paneTitle';
+  const mockedStripes = {
+    connect: jest.fn((component) => component),
+  };
+  const labelIds = {
+    generalTitle: 'ui-circulation.settings.title.general',
+    titleLevelRequestsTitle: 'ui-circulation.settings.title.titleLevelRequests',
+  };
+
+  beforeEach(() => {
+    render(
+      <TitleLevelRequests
+        stripes={mockedStripes}
+      />
+    );
+  });
+
+  afterEach(() => {
+    ConfigManager.mockClear();
+    mockedStripes.connect.mockClear();
+  });
+
+  it('should connect "ConfigManager" to stripes', () => {
+    expect(mockedStripes.connect).toHaveBeenLastCalledWith(ConfigManager);
+  });
+
+  it('should execute "ConfigManager" with passed props', () => {
+    const expectedResult = {
+      label: paneTitleLabelId,
+      moduleName: MODULE_NAMES.SETTINGS,
+      configName: CONFIG_NAMES.TLR,
+      configFormComponent: TitleLevelRequestsForm,
+      stripes: mockedStripes,
+      getInitialValues,
+      onBeforeSave: normalizeData,
+    };
+
+    expect(ConfigManager).toHaveBeenLastCalledWith(expectedResult, {});
+  });
+
+  it('should trigger TitleManager with correct props', () => {
+    const expectedProps = {
+      page: labelIds.generalTitle,
+      record: labelIds.titleLevelRequestsTitle,
+    };
+
+    expect(TitleManager).toHaveBeenCalledWith(expect.objectContaining(expectedProps), {});
+  });
+});
