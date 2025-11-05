996b5f93a00f963ad3a22ca4479817167ef14792
Marco Hamann <hamann@ub.uni-leipzig.de>
2025-11-05 10:28:18 +0100

    add holdRequestedForItem eventId

--- src/settings/NoticePolicy/utils/notice-description.test.js
+++ src/settings/NoticePolicy/utils/notice-description.test.js
@@ -18,6 +18,7 @@ const mockedData = {
     loanUserInitiatedEventsIds.RENEWED,
     loanUserInitiatedEventsIds.MANUAL_DUE_DATE_CHANGE,
     loanUserInitiatedEventsIds.ITEM_RECALLED,
+    loanUserInitiatedEventsIds.HOLD_REQUESTED_FOR_ITEM,
     requestItemStateChangeEventsIds.AVAILABLE,
     ...Object.values(requestUserInitiatedEventsIds),
     ...Object.values(requestTimeBasedEventsIds),
